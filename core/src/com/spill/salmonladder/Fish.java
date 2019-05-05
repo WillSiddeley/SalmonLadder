d51c3d79c7bdb6ed99a53bb56e189e82d47ca404
DeathyVoid <will.iams.sapo@gmail.com>
2019-05-05 14:58:45 -0400

    FISH GANG 2.0

--- core/src/com/spill/salmonladder/Fish.java
+++ core/src/com/spill/salmonladder/Fish.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
 
 public class Fish extends Actor {
 
@@ -18,19 +21,57 @@ public class Fish extends Actor {
 
         this.setOrigin(this.sprite.getWidth() / 2, this.sprite.getHeight() / 2);
 
+        this.setTouchable(Touchable.enabled);
+
+        this.addListener(new ActorGestureListener() {
+
+            @Override
+            public void fling(InputEvent event, float velocityX, float velocityY, int button) {
+
+                move(1);
+
+            }
+
+        });
+
     }
 
     @Override
-    public void draw(Batch batch, float alpha){
+    public void draw(Batch batch, float alpha) {
 
         sprite.draw(batch);
 
     }
 
     @Override
-    public void positionChanged(){
+    public void positionChanged() {
 
         sprite.setPosition(getX(), getY());
+
+    }
+
+    public void move(int direction) {
+
+        switch (direction) {
+
+            case 1:
+                setPosition(getX(), getY() + 16);
+                break;
+
+            case 2:
+                setPosition(getX(), getY() - 16);
+                break;
+
+            case 3:
+                setPosition(getX() + 16, getY());
+                break;
+
+            case 4:
+                setPosition(getX() - 16, getY());
+                break;
+
+
+        }
 
     }
 
--- core/src/com/spill/salmonladder/Scenes/LevelParser.java
+++ core/src/com/spill/salmonladder/Scenes/LevelParser.java
@@ -1,28 +1,19 @@
 package com.spill.salmonladder.Scenes;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.World;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
 import com.badlogic.gdx.utils.XmlReader;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.spill.salmonladder.Fish;
-
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
-
-import static com.badlogic.gdx.scenes.scene2d.actions.Actions.moveTo;
-import static com.badlogic.gdx.scenes.scene2d.actions.Actions.scaleTo;
 
 public class LevelParser implements Screen {
 
@@ -36,19 +27,16 @@ public class LevelParser implements Screen {
     // THE STATE INT IS USED FOR SWITCH AND COMPARISONS
     public static int state;
 
-    // CREATE A NEW WORLD AND STAGE
-    private World world;
+    // CREATE A NEW STAGE
     private Stage stage;
 
     // CREATE A TILED MAP VARIABLE THAT IS USED TO GENERATE THE LEVEL'S GRAPHICS
     private TiledMap tiledMap;
     private OrthogonalTiledMapRenderer tiledMapRenderer;
 
-    // CREATE A NEW SPRITE BATCH THAT DRAWS ONTO THE SCREEN
-    private SpriteBatch batch = new SpriteBatch();
-
     // CREATE A NEW CAMERA
-    private OrthographicCamera camera = new OrthographicCamera();
+    private OrthographicCamera camera;
+    private ExtendViewport viewport;
 
     // CREATE TEXTURES THAT WILL BE USED FOR BUTTONS (MOVEMENT, PAUSE, ETC.)
     private Texture texture;
@@ -56,21 +44,14 @@ public class LevelParser implements Screen {
     // DEBUG BOOLEAN USED FOR DEBUGGING ONLY.  SET TO TRUE TO BE ABLE TO SEE THE HITBOXES OF BUTTONS, AND OBJECTS
     private Boolean debug = true;
 
-    // LEVEL NUMBER VARIABLE IS USED TO DETERMINE THE MAP THAT WILL BE LOADED WHEN PARSED IN FROM SCREENLEVELSELECT
-    public static int levelNumber;
-
-    // FISH PLAYER
-    private Fish fish;
-
-    public LevelParser(int LevelNumber) {
+    LevelParser(int LevelNumber) {
 
         XmlReader.Element root = new XmlReader().parse(Gdx.files.internal("Levels.xml"));
         XmlReader.Element LevelAttributes = root.getChildByName("Level" + LevelNumber);
 
-        levelNumber = LevelNumber;
-
         // CAMERA
-        camera.setToOrtho(false, Gdx.graphics.getWidth() / 1.7f, Gdx.graphics.getHeight() / 1.7f);
+        camera = new OrthographicCamera(640, (640f) * Gdx.graphics.getHeight() / Gdx.graphics.getWidth());
+        camera.position.set(320, 320, 0);
         camera.update();
 
         // MAP PARSING
@@ -78,20 +59,13 @@ public class LevelParser implements Screen {
         tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
         //MapParser.parseMapLayers(world, tiledMap);
 
-        // PLAYER
-        //texture = new Texture("Skins/e.");
-
-        // HUD
-        //stage = new Stage(new ScreenViewport());
-        //InputTable.createInputTable(stage, debug);
-
     }
 
 
     @Override
     public void show() {
 
-        fish = new Fish("Sprites/salmon.png");
+        Fish fish = new Fish("Sprites/salmon.png");
         stage = new Stage(new ScreenViewport());
 
         stage.addActor(fish);
